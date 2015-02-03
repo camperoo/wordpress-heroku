10b0a7831e4296716b23bc478faf6a23f32e3479
Douglas Barton <douglas.barton@gmail.com>
2015-02-03 09:52:40 -0800

    Removes duplicate file directory

--- wp-content/themes/shopkeeper1.2b/Theme Files/shopkeeper/inc/shortcodes/visual-composer/vc_templates/vc_separator.php
+++
@@ -1,7 +0,0 @@
-<?php
-extract( shortcode_atts( array(
-	'accent_color' => '',
-	'el_class' => ''
-), $atts ) );
-
-echo '<div class="vc_content_hr '.$el_class.'" style="border-top-color:'.$accent_color.'"></div>';
